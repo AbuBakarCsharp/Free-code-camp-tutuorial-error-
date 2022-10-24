383aea7239e686d72a834e8b0103079ea77b64f0
AbuBakarCsharp <112962290+AbuBakarCsharp@users.noreply.github.com>
2022-10-24 21:14:52 +0500

    Update HowToProgramScript.cs

--- Assets/Scripts/HowToProgramScript.cs
+++ Assets/Scripts/HowToProgramScript.cs
@@ -5,28 +5,39 @@ using UnityEngine;
 public class HowToProgramScript : MonoBehaviour
 {
 
-    float speed = 5.5f;
+    //float speed = 5.5f;
 
-    double hunger = 9.9;
+    //double hunger = 9.9;
 
-    int health = 100;
+    //int health = 100;
 
-    string name = "Steve";
+    //string name = "Steve";
 
-    bool IsDead = true;
+    //bool IsDead = true;
 
     private void Start()
     {
         int a = 6;
         int b = 9;
 
+        StartCoroutine(blah());
+
         lol();
         LOL(5, 3);
     }
+
+    IEnumerator blah()
+    {
+        yield return new WaitForSeconds(9f);
+
+        print("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA");
+    }
+
     static void lol()
     {
         print("This is from PRINT");
     }
+
     static int LOL(int a, int b)
     {
         return a + b;
